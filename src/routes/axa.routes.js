96252d7850bef4a0a5c7c9edbf711a88ba549d86
Alejandro <ualonso@ikeasistencia.com>
2024-06-17 11:56:47 -0600

    Obtención de token de autenticacion

--- src/routes/axa.routes.js
+++ src/routes/axa.routes.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.post("/test",test)
 
-router.post("/authorized",(req,res)=>{
+router.post("/authorized",async (req,res)=>{
     const cookieValue = req.cookies; 
     console.log("cookieValue", cookieValue);
 
@@ -16,7 +16,33 @@ router.post("/authorized",(req,res)=>{
         res.send('https://sso-production.up.railway.app/login')
         return 
     }
+    
+    if(datos.code !== ''){
+        const tokenUrl = `https://visabenefits-auth-test.axa-assistance.us/oauth/token`;
+        const data = {
+            grant_type: 'authorization_code',
+            client_id: '00ZNI7ED2VfOZ4g2M4mgje81lg1EsqDE',
+            code: datos.code,
+            redirect_uri: 'https://qa.conciergeforplatinum.com',
+            code_verifier: cookieValue.auth_verification.code_verifier
+        };
 
+        console.log("Los datos a enviar son: ", data)
+
+        try {
+            const response = await axios.post(tokenUrl, querystring.stringify(data), {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            });
+            const tokens = response.data;
+            console.log(tokens)
+        } catch (error) {
+            console.error('Error al obtener el token:', error.response ? error.response.data : error.message);
+            res.status(500).json({ error: 'Failed to get token' });
+        }
+    }
+    
     res.send("OK")
 })
 
